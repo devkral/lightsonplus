889fbf5a6c873d254e5488cb3361ba31124fa2f1
klenamenis <klenamenis@users.noreply.github.com>
2015-02-04 11:20:31 +0100

    Update lightson+cmd.sh
    rewrite for lighter and better readable code, imho

--- lightson+cmd.sh
+++ lightson+cmd.sh
@@ -6,53 +6,25 @@
 # Description: extension to lightson+
 
 #realdisp
-realdisp="$(echo "$DISPLAY" | sed -e "s/\.[0-9]*$//" )"
+realdisp=`echo "$DISPLAY" | cut -d. -f1`
 inhibitfile="/tmp/lightsoninhibit-$UID-$realdisp"
 
-inhibit()
-{
-	if [ ! -e "$inhibitfile" ]; then
-    touch "$inhibitfile"
-	  echo "inhibit screensaver" | wall
-  else
-    echo "screensaver is already inhibited"
-  fi
+inhibit() {
+	[ ! -e "$inhibitfile" ] && (touch "$inhibitfile" && echo "inhibit screensaver" | wall) || echo "screensaver is already inhibited"
 }
-uninhibit()
-{
-	if [ -e "$inhibitfile" ]; then
-    rm "$inhibitfile"
-	  echo "uninhibit screensaver" | wall
-  else
-    echo "screensaver is already uninhibited"
-  fi
+uninhibit() {
+	[ -e "$inhibitfile" ] && (rm "$inhibitfile" && echo "uninhibit screensaver" | wall) || echo "screensaver is already uninhibited"
 }
 
-status()
-{
-	if [ -e "$inhibitfile" ]; then
-	  echo "screensaver is inhibited"
-  else
-    echo "screensaver is uninhibited"
-  fi
-}
-switch()
-{
-	if [ -f "$inhibitfile" ]; then
-		uninhibit
-	else
-		inhibit
-	fi
-}
-
-if [ "$1" = "" ]; then
-	switch
-elif [ "$1" = "on" ] || [ "$1" = "true" ] || [ "$1" = "yes" ]; then
-	inhibit
-elif [ "$1" = "off" ] || [ "$1" = "false" ] || [ "$1" = "no" ]; then
-	uninhibit
-elif [ "$1" = "status" ]; then
-	status
-else
-	echo -e "usage: $0 [command]\non: inhibits\noff: uninhibits\nstatus: print status\nnothing: switches"
-fi
+case $1 in
+	"" )
+		[ -f $inhibitfile ] && uninhibit || inhibit;;
+	"on" | "true" | "yes" )
+		inhibit;;
+	"off" | "false" | "no" )
+		uninhibit;;
+	"status" )
+		[ -e $inhibitfile ] && echo "screensaver is inhibited" || echo "screensaver is uninhibited";;
+	* )
+		echo -e "usage: $0 [command]\non: inhibits\noff: uninhibits\nstatus: print status\nnothing: switches";;
+esac
